0492aaa33b1fc0a86ea6ec494f5f9d6304a1a1d9
Stefanstud <stefan.krsteski@gmail.com>
2023-10-07 23:26:19 +0200

    added sg, least_sqrs, ridg_regr, fixed comp_loss

--- implementations.py
+++ implementations.py
@@ -1,19 +1,22 @@
 from utils import *
+from helpers import *
+
 
 def mean_squared_error_gd(y, tx, initial_w, max_iters, gamma):
-    """ Linear regression using gradient descent
+    """Linear regression using gradient descent
 
     Args:
         y: numpy array of shape = (N, )
         tx: numpy array of shape = (N, D)
         initial_w: numpy array of shape = (D, 1)
         max_iters: a scalar denoting the total number of iterations
-        gamma: a scalar denoting the step size 
+        gamma: a scalar denoting the step size
 
     Returns:
         w: numpy array of shape = (D, 1) corresponding to the last weight vector of the method
-        loss: the corresponding loss value 
+        loss: the corresponding loss value
     """
+
     # initialize w
     w = initial_w
     # initialize loss
@@ -21,36 +24,66 @@ def mean_squared_error_gd(y, tx, initial_w, max_iters, gamma):
     for n_iter in range(max_iters):
         # compute gradient
         gradient = compute_gradient_mse(y, tx, w)
-        # update weights 
-        w = w - gamma*gradient
+        # update weights
+        w = w - gamma * gradient
         # compute loss
         loss = compute_loss_mse(y, tx, w)
     return w, loss
 
-def mean_squared_error_sgd(y, tx, initial_w, max_iters, gamma):
-    """ Linear regression using stochastic gradient descent
-    """
-    raise NotImplementedError
+
+def mean_squared_error_sgd(y, tx, initial_w, max_iters, gamma=0.1):
+    """Linear regression using stochastic gradient descent"""
+
+    # initialize w
+    w = initial_w
+    # initialize loss
+    for n_iter in range(max_iters):
+        # iterate over batches
+        for y_batch, tx_batch in batch_iter(y, tx, batch_size=1, num_batches=1):
+            # compute gradient
+            grad, _ = compute_stoch_gradient(y_batch, tx_batch, w)
+            # update weights
+            w = w - gamma * grad
+            # compute loss
+            loss = compute_loss_mse(y, tx, w)
+
+        print(
+            "SGD iter. {bi}/{ti}: loss={l}, w0={w0}, w1={w1}".format(
+                bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]
+            )
+        )
+    return w, loss
+
 
 def least_squares(y, tx):
-    """ Least squares regression using normal equations
-    """
-    raise NotImplementedError
+    """Least squares regression using normal equations"""
+
+    X = np.dot(tx.T, tx)
+    Y = np.dot(tx.T, y)
+    w = np.linalg.solve(X, Y)
+    loss = compute_loss_mse(y, tx, w)
+
+    return w, loss
+
 
 def ridge_regression(y, tx, lambda_):
-    """ Ridge regression using normal equations
-    """
-    raise NotImplementedError
+    """Ridge regression using normal equations"""
+
+    reg = 2 * tx.shape[0] * lambda_ * np.identity(tx.shape[1])
+    X = np.dot(tx.T, tx) + reg
+    Y = np.dot(tx.T, y)
+    w = np.linalg.solve(X, Y)
+    loss = compute_loss_mse(y, tx, w)
+    return w, loss
+
 
 def logistic_regression(y, tx, initial_w, max_iters, gamma):
-    """ Logistic regression using gradient descent or SGD (y in {0,1})
-    """
+    """Logistic regression using gradient descent or SGD (y in {0,1})"""
     raise NotImplementedError
 
-def reg_logistic_regression(y, tx, lambda_ , initial_w, max_iters, gamma):
-    """ Regularized logistic regression using gradient descent or SGD (y in {0,1}, 
+
+def reg_logistic_regression(y, tx, lambda_, initial_w, max_iters, gamma):
+    """Regularized logistic regression using gradient descent or SGD (y in {0,1},
     with regularization term lambda*|w|^2)
     """
     raise NotImplementedError
-
-
--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
 import numpy as np
 
 """ Helper functions for mean_squared_error_gd"""
+
+
 def compute_loss_mse(y, tx, w):
     """Computes the loss using MSE.
 
@@ -12,13 +14,12 @@ def compute_loss_mse(y, tx, w):
     Returns:
         the value of the loss (a scalar), corresponding to the input parameters w.
     """
-    # obtain N
-    N = y.shape[0]
     # compute error e
-    e = y - np.matmul(tx, w)
+    e = y - np.dot(tx, w)
     # compute loss
-    loss = 1/(2*N) * np.matmul(e.T, e)
+    loss = 1 / 2 * np.mean(e**2)
     return loss
+
 
 def compute_gradient_mse(y, tx, w):
     """Computes the gradient at w.
@@ -31,10 +32,24 @@ def compute_gradient_mse(y, tx, w):
     Returns:
         An numpy array of shape (D, ) (same shape as w), containing the gradient of the loss at w.
     """
-    # obtain N
-    N = y.shape[0]
-    # compute e
-    e = y - np.matmul(tx, w)
-    # compute gradient
-    gradient = - 1/N * np.matmul(tx.T, e)
+
+    e = y - np.dot(tx, w)
+    gradient = -np.dot(tx.T, e) / len(e)
     return gradient
+
+
+def compute_stoch_gradient(y, tx, w):
+    """Compute a stochastic gradient at w from just few examples n and their corresponding y_n labels.
+
+    Args:
+        y: numpy array of shape=(N, )
+        tx: numpy array of shape=(N,2)
+        w: numpy array of shape=(2, ). The vector of model parameters.
+
+    Returns:
+        A numpy array of shape (2, ) (same shape as w), containing the stochastic gradient of the loss at w.
+    """
+
+    e = y - np.dot(tx, w)
+    gradient = -np.dot(tx.T, e) / len(e)
+    return gradient, e
