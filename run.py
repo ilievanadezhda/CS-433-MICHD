8a8e31d0ca28c4bbb8d3f88e97798b5e36f42e21
Stefanstud <stefan.krsteski@gmail.com>
2023-10-28 15:35:24 +0200

    drop_corr_threshold = 1, no val

--- run.py
+++ run.py
@@ -27,35 +27,32 @@ CONFIG = {
     "DROP_NAN_THRESHOLD": 1,
     "CAT_NUM_THRESHOLD": 200,
     "DROP_SINGLE": True,
-    "DROP_CORR_THRESHOLD": 0.9,
+    "DROP_CORR_THRESHOLD": 1,
     "REMOVE_OUTLIERS": False,
     "BUILD_POLY": False,
     "DEGREE": 2,
     "BUILD_LOG": False,
     "BUILD_RATIOS": False,
     "STANDARDIZE": True,
-    "SPLIT_RATIO": 0.9,
+    "SPLIT_RATIO": 1,
 }
 
 
 def load_and_split_data(path):
     x_train, x_test, Y_train, train_ids, test_ids = load_csv_data(path)
     split_index = int(len(x_train) * CONFIG["SPLIT_RATIO"])
-    X_train, X_val = x_train[:split_index], x_train[split_index:]
-    y_train, y_val = Y_train[:split_index], Y_train[split_index:]
+    X_train = x_train[:split_index]
+    y_train = Y_train[:split_index]
     y_train[np.where(y_train == -1)] = 0
-    y_val[np.where(y_val == -1)] = 0
-    return X_train, X_val, x_test, y_train, y_val, train_ids, test_ids
+    return X_train, x_test, y_train, train_ids, test_ids
 
 
-def preprocess_data(X_train, X_val, x_test):
+def preprocess_data(X_train, x_test):
     x_train_temp = X_train.copy()
-    x_val_temp = X_val.copy()
     x_test_temp = x_test.copy()
     x_train_temp, cols_to_keep_1 = drop_columns(
         x_train_temp, CONFIG["DROP_NAN_THRESHOLD"]
     )
-    x_val_temp = x_val_temp[:, cols_to_keep_1]
     x_test_temp = x_test_temp[:, cols_to_keep_1]
     print(
         f"Dropping columns with DROP_NAN_THRESHOLD = {CONFIG['DROP_NAN_THRESHOLD']}..."
@@ -75,14 +72,8 @@ def preprocess_data(X_train, X_val, x_test):
     x_train_temp[:, categorical_features] = median_imputation(
         x_train_temp[:, categorical_features]
     )
-    x_val_temp[:, categorical_features] = median_imputation(
-        x_val_temp[:, categorical_features]
-    )
     x_train_temp[:, numerical_features] = mean_imputation(
         x_train_temp[:, numerical_features]
-    )
-    x_val_temp[:, numerical_features] = mean_imputation(
-        x_val_temp[:, numerical_features]
     )
     x_test_temp[:, categorical_features] = median_imputation(
         x_test_temp[:, categorical_features]
@@ -95,56 +86,46 @@ def preprocess_data(X_train, X_val, x_test):
         low_perc = np.percentile(x_train_temp, 2, axis=0)
         high_perc = np.percentile(x_train_temp, 100 - 2, axis=0)
         x_train_temp = remove_outliers(x_train_temp, low_perc, high_perc)
-        x_val_temp = remove_outliers(x_val_temp, low_perc, high_perc)
         x_test_temp = remove_outliers(x_test_temp, low_perc, high_perc)
 
     # Data engineering: ratios, log-transform, polynomial features
     if CONFIG["BUILD_RATIOS"]:
         print("Building ratios...")
         ratio_features_train = build_ratios(x_train_temp[:, numerical_features])
-        ratio_features_val = build_ratios(x_val_temp[:, numerical_features])
         ratio_features_test = build_ratios(x_test_temp[:, numerical_features])
         x_train_temp = np.hstack((x_train_temp, ratio_features_train))
-        x_val_temp = np.hstack((x_val_temp, ratio_features_val))
         x_test_temp = np.hstack((x_test_temp, ratio_features_test))
 
     if CONFIG["BUILD_LOG"]:
         print("Building log...")
         log_features_train = build_log(x_train_temp[:, numerical_features])
-        log_features_val = build_log(x_val_temp[:, numerical_features])
         log_features_test = build_log(x_test_temp[:, numerical_features])
         x_train_temp = np.hstack((x_train_temp, log_features_train))
-        x_val_temp = np.hstack((x_val_temp, log_features_val))
         x_test_temp = np.hstack((x_test_temp, log_features_test))
 
     if CONFIG["BUILD_POLY"]:
         print("Building polynomial features...")
         x_train_temp = build_poly(x_train_temp, 2)
-        x_val_temp = build_poly(x_val_temp, 2)
         x_test_temp = build_poly(x_test_temp, 2)
-    print(x_train_temp.shape)
 
     # Drop columns with a single unique value or highly correlated columns
     if CONFIG["DROP_SINGLE"]:
         x_train_temp, cols_to_keep_3 = drop_single_value_columns(x_train_temp)
         x_test_temp = x_test_temp[:, cols_to_keep_3]
-        x_val_temp = x_val_temp[:, cols_to_keep_3]
         print("Dropping single valued columns...")
 
     x_train_temp, cols_to_keep_2 = drop_correlated_columns(
         x_train_temp, CONFIG["DROP_CORR_THRESHOLD"]
     )
-    x_val_temp = x_val_temp[:, cols_to_keep_2]
     x_test_temp = x_test_temp[:, cols_to_keep_2]
 
     # Standardize the datasets
     if CONFIG["STANDARDIZE"]:
         print("Standardizing...")
         x_train_temp = standardize(x_train_temp)
-        x_val_temp = standardize(x_val_temp)
         x_test_temp = standardize(x_test_temp)
 
-    return x_train_temp, x_val_temp, x_test_temp
+    return x_train_temp, x_test_temp
 
 
 def build_network(input_dim):
@@ -152,9 +133,8 @@ def build_network(input_dim):
     return network
 
 
-def train_and_evaluate(network, x_train_temp, x_val_temp, y_train, y_val):
+def train_and_evaluate(network, x_train_temp, y_train):
     train_log = []
-    val_log = []
 
     for epoch in range(5):
         for x_batch, y_batch in iterate_minibatches(
@@ -163,31 +143,25 @@ def train_and_evaluate(network, x_train_temp, x_val_temp, y_train, y_val):
             train(network, x_batch, y_batch)
 
         train_log.append(np.mean(predict(network, x_train_temp) == y_train))
-        val_log.append(np.mean(predict(network, x_val_temp) == y_val))
 
-        f1_val = f1_score(y_val, predict(network, x_val_temp))
         f1_train = f1_score(y_train, predict(network, x_train_temp))
 
         print("Epoch", epoch)
         print("Train accuracy:", train_log[-1])
         print("F1 score train:", f1_train)
-        print("Val accuracy:", val_log[-1])
-        print("F1 score val:", f1_val)
 
-    return train_log, val_log
+    return train_log
 
 
 def main():
-    X_train, X_val, x_test, y_train, y_val, train_ids, test_ids = load_and_split_data(
-        "../data"
-    )
-    x_train_temp, x_val_temp, x_test_temp = preprocess_data(X_train, X_val, x_test)
+    X_train, x_test, y_train, train_ids, test_ids = load_and_split_data("../data")
+    x_train_temp, x_test_temp = preprocess_data(X_train, x_test)
     network = build_network(x_train_temp.shape[1])
-    train_and_evaluate(network, x_train_temp, x_val_temp, y_train, y_val)
+    train_and_evaluate(network, x_train_temp, y_train)
 
     y_pred = predict(network, x_test_temp)
     y_pred[np.where(y_pred == 0)] = -1
-    create_csv_submission(test_ids, y_pred, "best_model.csv")
+    create_csv_submission(test_ids, y_pred, "best_model1.csv")
 
 
 if __name__ == "__main__":
